f6ab1b55e5c0d875a02a7c5cae2492e3b510edcf
Arpit <arpit.shrivastava2212@gmail.com>
2024-10-28 01:54:40 +0530

    chaged frontend

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -7,7 +7,7 @@ import { useRouter } from 'next/router';
 import { GiHamburgerMenu } from 'react-icons/gi';
 import { setUserData } from '@/Utils/UserSlice';
 import { AiFillCaretDown, AiFillCaretUp } from 'react-icons/ai';
-
+import Image from 'next/image'
 
 
 export default function NavBar() {
@@ -88,7 +88,8 @@ export default function NavBar() {
         <>
             <div className={`w-full ${scrolled ? "bg-indigo-600/70" : "bg-indigo-600"} px-6 h-20 bg-indigo-600 text-white flex items-center justify-between fixed top-0 left-0 z-50`}>
                 <div className='px-2 h-full flex items-center justify-center'>
-                    <p className='uppercase font-semibold tracking-widest text-lg'>JOB-PORTAL</p>
+                   
+                    <p className='uppercase font-semibold tracking-widest text-lg'>Work|Wave</p>
                 </div>
                 <div className='px-2 h-full hidden items-center justify-center lg:flex'>
                     <Link href={'/'} className="px-3 mx-4 text-base font-medium transition-all duration-700 hover:translate-y-2 uppercase" >Home</Link>
